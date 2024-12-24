e847b95d9b08a97a614cc1fdc12b1d00394c0c76
Urvashi tiwari <priyatiwari0425@gmail.com>
2024-12-24 14:02:00 +0530

    Add files via upload

---
+++ ArithmeticOperation.py
@@ -0,0 +1,7 @@
+x = (input("Enter the first number:"))
+y = (input("Enter the second number:"))
+print("Concatinating two number:", x+y)
+print("Addition of two number is:",int(x)+int(y))
+print("Subtraction of two number is:",int(x)-int(y))
+print("Multiplication of two number is:",int(x)*int(y))
+print("Division of two number is:",int(x)/int(y))
