bdd867fca11b4d0327a3276663d425ebc221c021
Urvashi tiwari <priyatiwari0425@gmail.com>
2024-12-26 20:02:20 +0530

    Add files via upload

---
+++ realNum.py
@@ -0,0 +1,9 @@
+number = int(input("Enter the number:"))
+if(number>0):
+    print("The given number is positive:",number)
+elif (number==0):
+    print("The given number is zero:",number)
+elif(number<0): 
+    print("The given number is negative:",number) 
+else:
+    print("Special character or number:")      
