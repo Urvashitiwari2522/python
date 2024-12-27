6e740867d57b8233218a03ce45ad808be272abe8
Urvashi tiwari <priyatiwari0425@gmail.com>
2024-12-27 23:37:58 +0530

    Add files via upload

---
+++ forLoop.py
@@ -0,0 +1,25 @@
+Number =int(input("Enter the value:"))
+# iterate i=0 to i=Number
+for i in range(0,Number+1):
+#    print(Number)
+#    print(i)
+#    table of 3
+   print(i*3)
+#    print the even number
+   if(i%2==0):
+      print("Number is even!")
+language ="Python"
+for i in language:
+    print(i)
+values = range(4)
+# iterate i=0 to i=3
+for i in range(4):
+    print(i)
+colors =['Green','Red','Yellow','Blue']
+for color in colors:
+    print(color)
+    for char in color:
+        print(char)
+
+for k in range(1,12,3):
+    print(k)
