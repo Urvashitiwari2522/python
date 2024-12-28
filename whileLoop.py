d36ec4aa8035d9c2fdb781bdf84b348c47b30299
Urvashi tiwari <priyatiwari0425@gmail.com>
2024-12-28 22:10:32 +0530

    Add files via upload

---
+++ whileLoop.py
@@ -0,0 +1,20 @@
+x = 1
+while (x<10):
+    print("Hello")
+    x=x+1
+value=1
+name = "Loop in python"
+while (value<len(name)):
+    value += value
+    pass
+    print(name)
+i=0
+name = "python"
+while (i<len(name)):
+    print(i)
+    i += 1
+# reverse while loop
+count = 10
+while (count>0):
+    print(count,end=",")
+    count =count-1
