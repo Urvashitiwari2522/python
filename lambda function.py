d45b64c04bb2ef7692002209e08801c0a3a846f4
Urvashi tiwari <priyatiwari0425@gmail.com>
2025-01-14 21:12:20 +0530

    Add files via upload

---
+++ lambda function.py
@@ -0,0 +1,21 @@
+# LAmbda function are often used  in situations where a small function is required  for a shorted period of time
+# they are commonly used as argument to higher-ordered functions such as map , filter, reduce
+# Using function
+def square(x):
+    return x*2
+print(square(2))
+# Using lambda 
+sq = lambda x: x*2
+print(sq(5))
+
+# cube using lambda
+cube = lambda x: x*x*x
+print(cube(3))
+
+# avg using lambda
+avg = lambda x,y,z: x+y+z/3
+print(avg(2,3,4))
+
+# percentage using lambda
+percentage = lambda totalMarks : (totalMarks/500)*100
+print(percentage(400))
